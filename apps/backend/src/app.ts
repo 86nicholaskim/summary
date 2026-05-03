83f0c7c0da7347a87bdfddaf90ecd99732a53cb8
seongbeom kim <86nicholaskim@gmail.com>
2026-05-03 20:23:06 +0900

    fix: favicon

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -7,4 +7,7 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Server is Running!' });
 });
 
+// 정적 파일 서빙 설정 (현재 폴더 기준 public 폴더 연결)
+app.use(express.static('public'));
+
 export default app; // server.ts에서 사용할 수 있도록 내보내기
