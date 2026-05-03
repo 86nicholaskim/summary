296cd5bcdd1c3647eb14872c39a71802d1d3eced
seongbeom kim <86nicholaskim@gmail.com>
2026-05-03 20:15:36 +0900

    fix: backend package.json scripts and engine

--- apps/backend/rollup.config.js
+++ apps/backend/rollup.config.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/app.ts', // 진입점
+  input: './src/server.ts', // 진입점
   output: {
     dir: 'dist',
     format: 'es', // nodenext에 맞게 ES 모듈로 출력
@@ -24,7 +24,7 @@ export default {
       // 3. 컴파일 결과물이 저장될 위치 명시
       outDir: 'dist',
       declaration: true,
-      declarationDir: 'dist/types',
+      declarationDir: './dist/types',
     }), // TS 설정 적용
   ],
   // node_modules의 외부 라이브러리는 번들에 포함하지 않고 런타임에 불러오도록 설정
